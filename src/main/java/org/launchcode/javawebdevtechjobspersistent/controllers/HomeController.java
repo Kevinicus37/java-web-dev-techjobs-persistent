3740cd9b236657d2a3d7068520495b6dbeba1077
Kevin Childs <kevinicus37@gmail.com>
2020-03-07 23:54:38 -0600

    Finished Part 3 - I think. Instructions are very poor and missing a lot.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,12 +13,22 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -26,6 +41,8 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -38,13 +55,27 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
+        Optional<Employer> result = employerRepository.findById(employerId);
+        if (result.isPresent()){
+            Employer employer = (Employer) result.get();
+            newJob.setEmployer(employer);
+            jobRepository.save(newJob);
 
+        }
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
 
+        Optional<Job> result = jobRepository.findById((jobId));
+        if (result.isPresent()){
+            Job job = (Job) result.get();
+            model.addAttribute("job", job);
+        }
+        else{
+            return "redirect:";
+        }
         return "view";
     }
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +13,10 @@ public class Employer extends AbstractEntity {
     @NotEmpty
     @Size(min=3, max = 50, message="Invalid location")
     private String location;
+
+    @OneToMany(mappedBy="employer")
+    private List<Job> jobs = new ArrayList<>();
+
 
     public Employer(){}
 
